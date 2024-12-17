b5cbccf9107b16674921760aca032c829a2cc990
Gaurav-Wankhede <pgywww@gmail.com>
2024-12-17 21:42:58 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -8,17 +8,16 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "localhost:3000",
-    "https://heroic-ats-frontend.vercel.app/",
-    "https://heroic-ats-platform-738207385737.us-central1.run.app/"
+    "https://heroic-ats-frontend.vercel.app",
+    "https://heroic-ats-platform-738207385737.us-central1.run.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,  # Specific origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"]   # Allow all headers
 )
 
 app.include_router(ats_router.router)
